3496e70675f5a6f2fe06ff6a3554f628ae27fd7b
Saeyoung Park <spark363@my.bcit.ca>
2025-01-11 13:03:31 -0800

    Fix file structure

--- COMP4537/labs/0/js/script.js
+++ COMP4537/labs/0/js/script.js
@@ -23,3 +23,43 @@
 // Otherwise, the moment the user clicked a button in wrong order, 
 // display "Wrong order!" message 
 // and reveal the correct order of all buttons and game ends.
+
+const colorArray = ["d00000", "ffba08", "3f88c5", "032b43", "136f63", "595959", "7f5539"]
+
+const number = document.getElementById("num").value
+
+// import random
+class Buttons {
+    constructor(color, order, shuffleXY) {
+        this.color = color;
+        this.order = order;
+        this.shuffleXY = shuffleXY;
+    }
+}
+
+function validateNum(number) {
+    if (number > 2 && number < 8) {
+        alert(number);
+    } else {
+        alert("else");
+    }
+}
+
+function makeBtn(number) {
+
+}
+
+function shuffleBtn(btnArr) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
