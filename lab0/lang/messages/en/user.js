f768e919865721617dbadb8f2237200d3d938bfa
Saeyoung Park <spark363@my.bcit.ca>
2025-01-10 16:47:38 -0800

    Set up texts

---
+++ lab0/lang/messages/en/user.js
@@ -0,0 +1,16 @@
+
+
+"How many buttons to create?"
+"Go!"
+"Number 3 ~7 only"
+"Type one of 3, 4, 5, 6, 7 only."
+"Memory game start"
+"Boxes'll display 00 seconds."
+"Game over"
+1
+2
+3
+4
+5
+6
+7
