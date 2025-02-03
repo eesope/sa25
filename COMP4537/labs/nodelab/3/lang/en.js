930b8d8b2cc51ab469ba6124401d5f1acd86e0c8
Saeyoung Park <spark363@my.bcit.ca>
2025-02-02 21:49:46 -0800

    Update to apply OOP

--- COMP4537/labs/nodelab/3/lang/en.js
+++ COMP4537/labs/nodelab/3/lang/en.js
@@ -1,8 +1,8 @@
 const greetingMSG = "Hello %1, What a beautiful day.";
-const BAD_404 = "404: Could not process the request.";
+const BAD_404 = "404: Unable to process the request; check the page link.";
 
 const file_part = {
-    no_access: `500: Server Error; could not access the requested file.`,
+    no_access: `500: Server Error; Unable to access the requested file.`,
     ok: `Text was processed successfully.`
 }
 
--- COMP4537/labs/nodelab/3/server.js
+++ COMP4537/labs/nodelab/3/server.js
@@ -2,32 +2,63 @@ const http = require('http'); // host as http
 const { getDate } = require('./modules/utils.js');
 const { greetingMSG, BAD_404 } = require('./lang/en.js');
 
+class Router {
+    constructor(req, res) {
+        this.req = req;
+        this.res = res;
+        this.url = req.url;
+    }
+
+    // routing upon url
+    route() {
+        switch (true) {
+            case this.url.includes('COMP4537/labs/3/getDate'):
+                this.handleGetDate();
+                break;
+            case this.url.includes('COMP4537/labs/3/readFile'):
+                this.handleReadFile();
+                break;
+            case this.url.includes('COMP4537/labs/3/writeFile'):
+                this.handleWriteFile();
+                break;
+            default:
+                this.handle404();
+                break;
+        }
+    }
+
+    handleGetDate() {
+        const queryObject = new URLSearchParams(this.url.split('?')[1]);
+        const name = queryObject.get('name') || 'Guest';
+        const dateTime = getDate();
+        const resMSG = `${greetingMSG.replace('%1', name)} Server current date and time is ${dateTime}`;
+
+        this.res.writeHead(200, { 'Content-type': 'text/html' });
+        this.res.write(`<p>Write your name after the URL such as: url/?name=thename</p>`);
+        this.res.end(`<p style="color: blue;">${resMSG}</p>`);
+    }
+
+    handleReadFile() {
+        this.res.writeHead(200, { 'Content-type': 'text/html' });
+        this.res.end(`<p>File read functionality is not yet implemented.</p>`);
+    }
+
+    handleWriteFile() {
+        this.res.writeHead(200, { 'Content-type': 'text/html' });
+        this.res.end(`<p>File write functionality is not yet implemented.</p>`);
+    }
+
+    handle404() {
+        this.res.writeHead(404, { 'Content-type': 'text/html' });
+        this.res.end(`<h1>${BAD_404}</h1>`);
+    }
+}
+
 const startServer = () => {
 
     const server = http.createServer((req, res) => {
-        const url = req.url;
-
-        if (url.includes('COMP4537/labs/3/getDate')) {
-
-            // check ?query
-            const queryObject = new URLSearchParams(url.split('?')[1]);
-            const name = queryObject.get('name') || 'Guest';
-
-            // get server time
-            const dateTime = getDate();
-            const resMSG = `${greetingMSG.replace('%1', name)} Server current date and time is ${dateTime}`;
-
-            // res header
-            res.writeHead(200, { 'Content-type': 'text/html' });
-            res.write(`<p>Write your name after the URL such as: url/?name=thename</p>`);
-            res.end(`<p style="color: blue;">${resMSG}</p>`);
-
-        } else {
-            res.writeHead(404, { 'Content-type': 'text/html' });
-            res.end(`<h1>${BAD_404}</h1>`);
-        }
-
-
+        const router = new Router(req, res);
+        router.route();
     });
 
     server.listen(8080, () => {
