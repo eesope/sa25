13226870b933f3b9307de4271607a602569d4f09
Saeyoung Park <spark363@my.bcit.ca>
2025-02-02 21:17:15 -0800

    Add routing setting

--- COMP4537/labs/nodelab/3/server.js
+++ COMP4537/labs/nodelab/3/server.js
@@ -1,24 +1,33 @@
 const http = require('http'); // host as http
 const { getDate } = require('./modules/utils.js');
-const { greetingMSG } = require('./lang/en.js');
+const { greetingMSG, BAD_404 } = require('./lang/en.js');
 
 const startServer = () => {
 
     const server = http.createServer((req, res) => {
         const url = req.url;
 
-        // check ?query
-        const queryObject = new URLSearchParams(url.split('?')[1]);
-        const name = queryObject.get('name') || 'Guest';
+        if (url.includes('COMP4537/labs/3/')) {
 
-        // get server time
-        const dateTime = getDate();
-        const resMSG = `${greetingMSG.replace('%1', name)} Server current date and time is ${dateTime}`;
+            // check ?query
+            const queryObject = new URLSearchParams(url.split('?')[1]);
+            const name = queryObject.get('name') || 'Guest';
 
-        // res header
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.write(`<p>Write your name after the URL such as: url/?name=thename</p>`);
-        res.end(`<p style="color: blue;">${resMSG}</p>`);
+            // get server time
+            const dateTime = getDate();
+            const resMSG = `${greetingMSG.replace('%1', name)} Server current date and time is ${dateTime}`;
+
+            // res header
+            res.writeHead(200, { 'Content-type': 'text/html' });
+            res.write(`<p>Write your name after the URL such as: url/?name=thename</p>`);
+            res.end(`<p style="color: blue;">${resMSG}</p>`);
+
+        } else {
+            res.writeHead(404, { 'Content-type': 'text/html' });
+            res.end(`<h1>${BAD_404}</h1>`);
+        }
+
+
     });
 
     server.listen(8080, () => {
--- COMP4537/labs/nodelab/index.js
+++ COMP4537/labs/nodelab/index.js
@@ -5,4 +5,4 @@ const { startServer } = require('./3/server.js');
 // http starting on server module
 startServer();
 
-console.log("Listening...\nServer is running on http://localhost:8080 at the moment.");
+console.log("Listening...\nServer is running.");
